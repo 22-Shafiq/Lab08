78f34bb6ca8f40d4efe5bff4afbd6597da2eb143
22-Shafiq <2001biscuit@gmail.com>
2021-10-20 11:06:21 +0800

    Score Condition Implemented

---
+++ Assets/ScoreCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCollector : MonoBehaviour
+{
+    public Text scoreText;
+
+    private int ScoreCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreCounter = 0;
+        scoreText.text = "Score: " + ScoreCounter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Obstacle")
+        {
+            ScoreCounter += 1;
+            scoreText.text = "Score: " + ScoreCounter;
+
+        }
+    }
+}
